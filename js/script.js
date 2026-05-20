263be953cdd07b985e31942904b188c8baae52ed
epslon316 <Othnielarmel@gmail.com>
2026-05-20 15:39:57 +0000

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Message de bienvenue dans la console pour vérifier que le fichier est bien lié
+console.log("Le fichier JavaScript est correctement chargé !");
+
+// Petite fonction interactive pour saluer les visiteurs
+function saluerVisiteur() {
+    alert("Bonjour et bienvenue sur mon portfolio d'Analyste Statisticien !");
+}
